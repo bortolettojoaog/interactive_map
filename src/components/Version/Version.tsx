533fa40eda5018f7c218c543789a537cc4cc81f9
João G. Bortoletto <jgb3532@hotmail.com>
2026-03-05 10:09:36 -0300

    feat: create version component

---
+++ src/components/Version/Version.tsx
@@ -0,0 +1,11 @@
+import './Version.css';
+
+function Version() {
+    return (
+        <div className="version-footer">
+            v1.0.0 powered by <b>BortolettoTech</b>
+        </div>
+    );
+}
+
+export default Version;
